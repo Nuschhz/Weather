495d5acfc3639ccef447ac1e27cda13cfa0eea97
Nuschhz <nusch05@hotmail.com>
2023-08-31 15:53:10 -0300

    commit

--- src/hooks/useGetWeather.js
+++ src/hooks/useGetWeather.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import APIKEY from "../apikey";
 
 export const useGetWeather = (place = "santos,br", units = "metric") => {
   const [data, setData] = useState(() => {});
   useEffect(() => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=2c92e961a68bf34adb581ef4a9b0aae8&units=${units}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=${APIKEY}&units=${units}`;
     axios
       .get(url)
       .then((res) => setData(res))
